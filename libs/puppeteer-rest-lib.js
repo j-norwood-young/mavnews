a1557e0edf29d8687918778ce15f756e5dba1366
Jason Norwood-Young <jason@10layer.com>
2017-12-11 09:18:20 +0200

    Get puppeteer username and password from config file

--- libs/puppeteer-rest-lib.js
+++ libs/puppeteer-rest-lib.js
@@ -3,20 +3,18 @@ var config = require("Config");
 
 class PuppeteerRestLib {
 	constructor(opts) {
-		this.username = opts.username;
-		this.password = opts.password;
 	}
 
 	open(url, reload) {
-		return rest.post(`${ config.puppeteerRest.url }/open`, { username: this.username, password: this.password, data: { url, reload }})
+		return rest.post(`${ config.puppeteerRest.url }/open`, { username: config.puppeteerRest.username, password: config.puppeteerRest.password, data: { url, reload }})
 	}
 
 	text(id, selector) {
-		return rest.post(`${ config.puppeteerRest.url }/page/${ id }/text`, { username: this.username, password: this.password, data: { selector }})
+		return rest.post(`${ config.puppeteerRest.url }/page/${ id }/text`, { username: config.puppeteerRest.username, password: config.puppeteerRest.password, data: { selector }})
 	}
 
 	attr(id, selector, attr) {
-		return rest.post(`${ config.puppeteerRest.url }/page/${ id }/attr`, { username: this.username, password: this.password, data: { selector, attr }})
+		return rest.post(`${ config.puppeteerRest.url }/page/${ id }/attr`, { username: config.puppeteerRest.username, password: config.puppeteerRest.password, data: { selector, attr }})
 	}
 }
 
